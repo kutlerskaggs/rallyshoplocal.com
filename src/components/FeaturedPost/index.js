302fa1987483c13e0c67e440a6da8aafcf1fc255
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-05-01 14:59:50 -0600

    more work on feature cards

--- src/components/FeaturedPost/index.js
+++ src/components/FeaturedPost/index.js
@@ -3,6 +3,8 @@ import React, { Component, PropTypes } from 'react'
 import styles from './styles.scss'
 // HOC
 import Window from 'HOC/Window'
+// utils
+import moment from 'moment'
 
 export class FeaturedPost extends Component {
 
@@ -34,14 +36,26 @@ export class FeaturedPost extends Component {
     let featuredImage = `${post.featured_image}?resize=600%2C600`
     let el = document.createElement('div')
     el.innerHTML = post.excerpt // TODO grabbing image caption ... remove?
-    let contentPreview = `${el.textContent.slice(0, 150)}...`
+    let contentPreview = `${el.textContent.slice(0, 200)}...`
     let cardImageStyle = { backgroundImage: `url('${featuredImage}')` }
     let showRevealStyle = { transform: 'translate3d(0, 0, 0)' }
 
     // TODO split title, concat into multiple headers
-    let title = post.title.split(' ').reduce((word) => {
+    let title = post.title.split(' ').reduce((output, word) => {
+      let lines = output.length
+      let currentLine = output[lines - 1]
+      if ((currentLine.length + word.length) < 15) {
+        currentLine += currentLine.length ? ` ${word}` : word
+        output[lines - 1] = currentLine
+      } else {
+        output.push(word)
+      }
+      return output
+    }, [''])
 
-    })
+    let _title = title.map((line, index) => <h3 key={index}>{line}</h3>)
+    let author = post.author.name.split(' ')
+    author = author.length > 1 ? `${author[0][0]}. ${author[1]}` : author.join(' ')
 
     return (
       <div className={`${styles.wrapper} ${styles[position]}`} onClick={this.onClick}>
@@ -54,14 +68,19 @@ export class FeaturedPost extends Component {
 
         <div className={styles.content}>
           <div className={styles.title}>
-            {title}
+            <div>
+              {_title}
+            </div>
+            <h6>{`${author} | ${moment(post.date).format('MMM D, YYYY')}`}</h6>
           </div>
           <div className={styles.reveal} style={this.state.reveal ? showRevealStyle : {}}>
-            <p><span>{contentPreview}</span></p>
-            <a className={styles.more} onClick={onClick}>
-              Read more
-              <i className='fa fa-sign-in fa-fw'></i>
-            </a>
+            <div>
+              <p><span>{contentPreview}</span></p>
+              <a className={styles.more} onClick={onClick}>
+                Read more
+                <i className='fa fa-sign-in fa-fw'></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
